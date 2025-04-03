46c0c39495d3a4c8d31ba0c39c3c842e59fe53a1
Moiz Ul Hasan <moizulhasan21@gmail.com>
2025-04-03 08:53:44 +0500

    add post list view

---
+++ SocialTwitter-VIPER/App/SocialTwitter_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialTwitter_VIPERApp.swift
+//  SocialTwitter-VIPER
+//
+//  Created by EBA on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialTwitter_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PostListViewRouterImpl.createPostListView()
+        }
+    }
+}
---
+++ SocialTwitter-VIPER/Modules/PostList/PostListView.swift
@@ -0,0 +1,47 @@
+//
+//  PostListView.swift
+//  SocialTwitter-VIPER
+//
+//  Created by EBA on 03/04/2025.
+//
+
+import SwiftUI
+
+struct PostListView: View {
+    @ObservedObject var presenter: PostListViewPresenterImpl
+    
+    init(presenter: PostListViewPresenterImpl) {
+        self.presenter = presenter
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(presenter.posts.indices, id: \.self) { index in
+                        PostView(post: presenter.posts[index])
+                        
+                        if index != presenter.posts.count - 1 {
+                            Divider()
+                        }
+                        
+                    }
+                }
+            }
+            .background(Color.blueThemeTwitter)
+        }
+        .onAppear {
+            presenter.onAppear()
+        }
+    }
+}
+
+fileprivate class PreviewPostListRouter: PostListRouter {
+    func navigateToCreatePost() {}
+}
+fileprivate class PreviewPostListInteractorInput: PostListInteractorInput {
+    func fetchPosts() {}
+}
+#Preview {
+    PostListView(presenter: .init(router: PreviewPostListRouter(), interactor: PreviewPostListInteractorInput()))
+}
